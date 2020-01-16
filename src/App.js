54dbf34de334f493b40001c9e48f8b4b7555214c
Chandrakant <chandrakantsavvy@gmail.com>
2020-01-16 12:35:04 +0530

    handled add todo to print in console

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import TodoList from './TodoList';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const todoNameRef = useRef;
+  const todoNameRef = useRef();
 
   function handleAddTodos() {
-
+    const name = todoNameRef.current.value;
+    if (name === '') return;
+    console.log(name);
+    todoNameRef.current.value = null;
   }
 
   return (
     <>
       <TodoList todos={todos} />
       <input ref={todoNameRef} type="text" />
-      <button onClick="{handleAddTodos}">Add Todo</button>
+      <button onClick={handleAddTodos}>Add Todo</button>
       <button>Clear completed todo</button>
       <div>0 todos left</div>
     </>
