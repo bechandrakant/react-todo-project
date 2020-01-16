696cf2748f13241595e0df9105cc5eb255cd7a88
Chandrakant <chandrakantsavvy@gmail.com>
2020-01-16 11:52:50 +0530

    done Todo with hardcoded values

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 
 function App() {
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2']);
+
   return (
-    <div>Hello World</div>
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear completed todo</button>
+      <div>0 todos left</div>
+    </>
   );
 }
 
