95f1fd28396bee66e3a74d2fa5f893f64aee80d8
Chandrakant <chandrakantsavvy@gmail.com>
2020-01-16 12:21:55 +0530

    removed hardcoded todos

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import TodoList from './TodoList';
 
 function App() {
-  const [todos, setTodos] = useState([{ id: 1, name: "Todo 1", complete: false },
-  { id: 2, name: "Todo 2", complete: true }]);
+  const [todos, setTodos] = useState([]);
+
+  function handleAddTodos() {
+
+  }
 
   return (
     <>
       <TodoList todos={todos} />
       <input type="text" />
-      <button>Add Todo</button>
+      <button onClick="{handleAddTodos}">Add Todo</button>
       <button>Clear completed todo</button>
       <div>0 todos left</div>
     </>
