4dd2835d55e91ca190146875b3da62e4ea6c67d9
Chandrakant <chandrakantsavvy@gmail.com>
2020-01-16 13:12:54 +0530

    stored todo values in local storage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList';
+import uuidv4 from 'uuid/v4';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
 
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
   function handleAddTodos() {
     const name = todoNameRef.current.value;
     if (name === '') return;
     setTodos(prevTodos => {
-      return [...prevTodos, { id: 1, name: name, complete: false }];
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }];
     })
     todoNameRef.current.value = null;
   }
